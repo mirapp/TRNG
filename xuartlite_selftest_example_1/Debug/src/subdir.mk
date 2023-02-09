cb5920d9a44104477da1255249a99d7786e5fb68
mirapp <92997245+mirapp@users.noreply.github.com>
2023-02-09 13:28:51 +0100

    Add files via upload

---
+++ xuartlite_selftest_example_1/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LD_SRCS += \
+../src/lscript.ld 
+
+C_SRCS += \
+../src/xuartlite_selftest_example.c 
+
+OBJS += \
+./src/xuartlite_selftest_example.o 
+
+C_DEPS += \
+./src/xuartlite_selftest_example.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM v7 gcc compiler'
+	arm-none-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -mcpu=cortex-a9 -mfpu=vfpv3 -mfloat-abi=hard -IC:/Users/mirap/Vivado/neoTRNG3/neoTRNG_platform/export/neoTRNG_platform/sw/neoTRNG_platform/standalone_domain/bspinclude/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
